77048972ccfbc1870de3beb4032a4050ae20a96e
Jean-Louis BOLI <blaaz20004@gmail.com>
2024-04-23 21:14:22 +0000

    Update, rest : defilement horizontale

--- js/main.js
+++ js/main.js
@@ -41,10 +41,11 @@
             $('.back-to-top').fadeOut('slow');
         }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+
+    // $('.back-to-top').click(function () {
+    //     $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    //     return false;
+    // }); 
 
 
     // Testimonials carousel
